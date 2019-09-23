8e8263acbdc98132ba9749cba5b381be57aeea7d
Jonathan Green <honda4546@gmail.com>
2019-09-23 13:00:31 -0700

    #include steamapi

--- Code/Plugin.h
+++ Code/Plugin.h
@@ -6,6 +6,53 @@
 
 #include <CryEntitySystem/IEntityClass.h>
 
+//steam api
+#include <steam/steam_api.h>
+
+//normal steam stuff
+#include <steam/isteamapplist.h>
+#include <steam/isteamapps.h>
+#include <steam/isteamappticket.h>
+#include <steam/isteamclient.h>
+#include <steam/isteamcontroller.h>
+#include <steam/isteamfriends.h>
+#include <steam/isteamgamecoordinator.h>
+#include <steam/isteamgameserver.h>
+#include <steam/isteamgameserverstats.h>
+#include <steam/isteamhtmlsurface.h>
+#include <steam/isteamhttp.h>
+#include <steam/isteaminput.h>
+#include <steam/isteaminventory.h>
+#include <steam/isteammasterserverupdater.h>
+#include <steam/isteammatchmaking.h>
+#include <steam/isteammusic.h>
+#include <steam/isteammusicremote.h>
+#include <steam/isteamnetworking.h>
+#include <steam/isteamnetworkingsockets.h>
+#include <steam/isteamnetworkingutils.h>
+#include <steam/isteamparentalsettings.h>
+#include <steam/isteamps3overlayrenderer.h>
+#include <steam/isteamremoteplay.h>
+#include <steam/isteamremotestorage.h>
+#include <steam/isteamscreenshots.h>
+#include <steam/isteamugc.h>
+#include <steam/isteamuser.h>
+#include <steam/isteamutils.h>
+#include <steam/isteamvideo.h>
+#include <steam/matchmakingtypes.h>
+#include <steam/steam_api_common.h>
+#include <steam/steam_api_internal.h>
+#include <steam/steam_gameserver.h>
+#include <steam/steamclientpublic.h>
+#include <steam/steamdatagram_tickets.h>
+#include <steam/steamencryptedappticket.h>
+#include <steam/steamhttpenums.h>
+#include <steam/steamnetworkingtypes.h>
+#include <steam/steamps3params.h>
+#include <steam/steamtypes.h>
+#include <steam/steamuniverse.h>
+
+
 class CPlugin 
 	: public Cry::IEnginePlugin
 	, public ISystemEventListener
