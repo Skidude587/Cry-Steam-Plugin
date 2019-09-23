eb28ed37b297b2321e2d289e89d5ccaaf026743d
Von Bismarck <omarocasio1945@gmail.com>
2019-09-23 16:03:06 -0700

    wait

---
+++ Code/lobby/SteamLobby.h
@@ -0,0 +1,27 @@
+/*
+
+
+	This Class file is for calling Steam API for our lobby system
+
+
+
+	Authors: Von Bismarck , 
+
+
+
+
+
+*/
+
+
+
+#pragma once 
+#include "StdAfx.h"
+
+#include "public/steam/steam_api.h"
+
+
+class CSteamLobbySystem
+{
+	
+};
