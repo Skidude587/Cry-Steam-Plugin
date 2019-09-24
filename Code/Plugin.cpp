7a42fbc546890bd5c5cfd1a9529330ca890fcfe1
Von Bismarck <omarocasio1945@gmail.com>
2019-09-23 18:42:10 -0700

    change define

--- Code/Plugin.cpp
+++ Code/Plugin.cpp
@@ -61,7 +61,7 @@ void CPlugin::OnSystemEvent(ESystemEvent event, UINT_PTR wparam, UINT_PTR lparam
 		// Listen for client connection events, in order to create the local player
 		gEnv->pGameFramework->AddNetworkedClientListener(*this);
 
-#ifdef USING_STEAM
+#if USING_STEAM
 		if (!SteamInput()->Init())
 		{
 			CRY_ASSERT("Steam needs to be running");
