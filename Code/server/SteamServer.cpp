825d76129a79ab75c695cc8d7909986372afdb82
Joey <42626374+Ruflex2@users.noreply.github.com>
2019-09-23 20:50:06 -0700

    Packet availability & reading

--- Code/server/SteamServer.cpp
+++ Code/server/SteamServer.cpp
@@ -1,18 +1,58 @@
 //
-//	File: SteamServer.cpp
-//	
+// -------------------------------------------------------------------------
+//  File name:   SteamServer.cpp
+//  Created:     23/09/2019 by Joey Gonzalez, 
+//  Description: Handles P2P sessions for sending/recieving packets
+// -------------------------------------------------------------------------
 //
-//
+////////////////////////////////////////////////////////////////////////////
 
 #include "StdAfx.h"
 #include "SteamServer.h"
 #include "lobby/steam_int_h.h"
 #include "public/steam/steam_api.h"
 
+// TODO: add log calls
+
+// Check if theres a packet ready to be read.
+bool cSteamServer::IsP2PPacketAvailable(uint32* pMsgSize)
+{
+	if (!SteamNetworking)
+	{
+		// Add error log ...
+
+		return false;
+	}
+
+	return SteamNetworking()->IsP2PPacketAvailable(pMsgSize);
+}
+
+// Recieve a packet along with its size and the associated user's steamID.
+// Returns false if there is no packet available to read, make sure to call
+// IsP2PPacketAvailable() prior to ReadP2PPacket. Returns true if a packet
+// Is sucessfully read.
+bool cSteamServer::ReadP2PPacket(void* pDest, uint32 destSize, uint32* pMsgSize, CSteamID* pSteamIDRemote)
+{
+	if (!SteamNetworking)
+	{
+		// Add error log ...
+
+		return false;
+	}
+
+	return SteamNetworking()->ReadP2PPacket(pDest, destSize, pMsgSize, pSteamIDRemote);
+}
 
 // Close P2P session with associated steamID.
 void cSteamServer::CloseP2PSession(CSteamID steamIDRemote)
 {
+	if (!SteamNetworking)
+	{
+		// Add error log ...
+
+		return;
+	}
+
 	if (!SteamNetworking()->CloseP2PSessionWithUser(steamIDRemote))
 	{
 		// No connection ever opened on that steamID.
@@ -24,9 +64,17 @@ void cSteamServer::CloseP2PSession(CSteamID steamIDRemote)
 // Callback for recieving P2P requests.
 void cSteamServer::OnP2PSessionRequest(P2PSessionRequest_t* pCallback)
 {
+	if (!SteamNetworking)
+	{
+		// Add error log ...
+
+		return;
+	}
+
 	// If returned false, steamID is invalid. 
 	if (!SteamNetworking()->AcceptP2PSessionWithUser(pCallback->m_steamIDRemote))
 	{
+		// SteamID is invalid.
 		// Add error log ...
 
 	}
--- Code/server/SteamServer.h
+++ Code/server/SteamServer.h
@@ -1,8 +1,11 @@
 //
-//	File: SteamServer.h
-//	
+// -------------------------------------------------------------------------
+//  File name:   SteamServer.h
+//  Created:     23/09/2019 by Joey Gonzalez, 
+//  Description: Class header for P2P sessions
+// -------------------------------------------------------------------------
 //
-//
+////////////////////////////////////////////////////////////////////////////
 
 #pragma once 
 #include "StdAfx.h"
@@ -13,7 +16,8 @@ class cSteamServer
 {
 public:
 	void CloseP2PSession(CSteamID steamIDRemote);
-
+	bool IsP2PPacketAvailable(uint32* pMsgSize);
+	bool ReadP2PPacket(void* pDest, uint32 destSize, uint32* pMsgSize, CSteamID* pSteamIDRemote);
 private:
 	STEAM_CALLBACK(cSteamServer, OnP2PSessionRequest, P2PSessionRequest_t);
 	STEAM_CALLBACK(cSteamServer, OnP2PSessionConnectFail, P2PSessionConnectFail_t);
