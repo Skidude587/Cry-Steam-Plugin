65a5efd8a412d879c38096fd4f2626c3b1a97ca4
Jonathan <honad4546@gmail.com>
2019-09-23 18:01:35 -0700

    Update SteamLobby.cpp

--- Code/lobby/SteamLobby.cpp
+++ Code/lobby/SteamLobby.cpp
@@ -9,14 +9,18 @@
 #include "StdAfx.h"
 #include "SteamLobby.h"
 #include "steam_int_h.h"
+#include "public/steam/steam_api.h"
+
 //Calls up Steam API lib
 #if USING_STEAM
 #pragma comment(lib, "steam_api64.lib")
 S_API bool S_CALLTYPE SteamAPI_Init();
-#include "CryLobby.h"
+//
 
+#include "CryLobby.h"
 //int LobbyCVars for steam lobbies 
 	int useSteamAsOnlineLobby;
 	int resetSteamAchievementsOnBoot;
 	int lobbySteamOnlinePort;
 #endif
+//
