db61da9575cfe2c1d4ba0b7a1c713a8645f889c9
Jonathan <honad4546@gmail.com>
2019-09-23 17:48:09 -0700

    Update

--- Code/lobby/SteamLobby.cpp
+++ Code/lobby/SteamLobby.cpp
@@ -2,16 +2,24 @@
 // -------------------------------------------------------------------------
 //  File name:   SteamLobby.cpp
 //  Created:     23/09/2019 by Jonathan Green, 
-//  Description: Creating the Steam Lobby
+//  Description: Creating the Multiplayer Steam Lobby
 // -------------------------------------------------------------------------
 //
 ////////////////////////////////////////////////////////////////////////////
 #include "StdAfx.h"
 #include "SteamLobby.h"
 
+//Calls up Steam API lin
 #ifndef STEAMLOBBY_H
 #define STEAMLOBBY_H
 #pragma comment(lib, "steam_api64.lib")
 
 #include "CryLobby.h"
+
+//int LobbyCVars for steam lobbies 
+#if USING_STEAM
+	int useSteamAsOnlineLobby;
+	int resetSteamAchievementsOnBoot;
+	int lobbySteamOnlinePort;
+#endif
 #endif
