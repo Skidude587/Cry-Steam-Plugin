18a146ebe20336a93a2dd334d9bb6b9fe78bd20c
Jonathan <honad4546@gmail.com>
2019-09-23 17:36:22 -0700

    SteamLobby First UP

---
+++ Code/lobby/SteamLobby.cpp
@@ -0,0 +1,17 @@
+//
+// -------------------------------------------------------------------------
+//  File name:   SteamLobby.cpp
+//  Created:     23/09/2019 by Jonathan Green, 
+//  Description: Creating the Steam Lobby
+// -------------------------------------------------------------------------
+//
+////////////////////////////////////////////////////////////////////////////
+#include "StdAfx.h"
+#include "SteamLobby.h"
+
+#ifndef STEAMLOBBY_H
+#define STEAMLOBBY_H
+#pragma comment(lib, "steam_api64.lib")
+
+#include "CryLobby.h"
+#endif
