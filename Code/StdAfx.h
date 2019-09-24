7f4aabecfa42dac7586e14b52c3cd9f177c41ab7
Von Bismarck <omarocasio1945@gmail.com>
2019-09-23 18:16:26 -0700

    Added new code to stdAfx to call steam Init()

--- Code/StdAfx.h
+++ Code/StdAfx.h
@@ -16,3 +16,14 @@
 #include "public/steam/isteamremotestorage.h"
 #include "public/steam/isteammatchmaking.h"
 #include "public/steam/steam_gameserver.h"
+
+
+#ifdef STEAM_CEG
+// Steam DRM header file
+#include "cegclient.h"
+#else
+#define Steamworks_InitCEGLibrary() (true)
+#define Steamworks_TermCEGLibrary()
+#define Steamworks_TestSecret()
+#define Steamworks_SelfCheck()
+#endif
