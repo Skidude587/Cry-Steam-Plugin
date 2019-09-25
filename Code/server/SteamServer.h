5b4b8ef3872c08da48ab420e177623ef6fda41a6
Skidude587 <35932750+Skidude587@users.noreply.github.com>
2019-09-24 17:55:08 -0700

    Update

--- Code/server/SteamServer.h
+++ Code/server/SteamServer.h
@@ -83,12 +83,7 @@ struct IServer
 	//! Gets the port that clients should connect to
 	virtual uint16 GetPort() const = 0;
 	//! Checks whether or not the server expects clients to connect with the server's local IP (LAN)
-	virtual bool IsLocal() const = 0;
-	//! Authenticates a specific user on the server using an auth token generated on the client using IPlugin::GetAuthToken.
-	//! \param clientIP Publicly accessibly IP address of the specific user
-	//! \param szAuthData Base address of the auth token
-	//! \param authDataLength Length of the auth token
-	//! \param userId Assigned on success to the user's platform-specific identifier
+	virtual bool IsLocal() const = 0; 
 	//! \returns True if the authentication was successful, otherwise false
 	virtual bool AuthenticateUser(uint32 clientIP, char* szAuthData, int authDataLength, AccountIdentifier& userId) = 0;
 	//! Should be called whenever a user leaves the game server, allowing the platform services to track what users are on which servers.
