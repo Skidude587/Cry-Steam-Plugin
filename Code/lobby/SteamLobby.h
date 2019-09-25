20a2b8b3c36ab169ab6876699304143223bdcac0
Jonathan Green <honda4546@gmail.com>
2019-09-25 09:16:13 -0700

    Update SteamLobby.h

--- Code/lobby/SteamLobby.h
+++ Code/lobby/SteamLobby.h
@@ -16,125 +16,8 @@
 #include "public/steam/steam_api.h"
 #include "public/steam/steam_gameserver.h"
 #include <../../CryEngine/CryCommon/CrySystem/ISystem.h>
-
-#define CRYLOBBY_USER_NAME_LENGTH        32
-#define CRYLOBBY_USER_GUID_STRING_LENGTH 40
-
-struct SCryUserID : public CMultiThreadRefCount
-{
-	virtual bool                                              operator==(const SCryUserID& other) const = 0;
-	virtual bool                                              operator<(const SCryUserID& other) const = 0;
-
-	virtual CryFixedStringT<CRYLOBBY_USER_GUID_STRING_LENGTH> GetGUIDAsString() const
-	{
-		return CryFixedStringT<CRYLOBBY_USER_GUID_STRING_LENGTH>("");
-	}
-};
-#define TMemHdl                     LobbyTMemHdl
-typedef void* TMemHdl;
-#define TMemInvalidHdl NULL
-
-class CCryLobby;
-
-struct SCrySharedUserID : public SCryUserID
-{
-	void* operator new(size_t size) throw()
-	{
-		CCryLobby* pLobby = (CCryLobby*)CCryLobby::GetLobby();
-
-		if (pLobby)
-		{
-			TMemHdl hdl = pLobby->MemAlloc(size);
-
-			if (hdl != TMemInvalidHdl)
-			{
-				SCrySharedUserID* id = (SCrySharedUserID*)pLobby->MemGetPtr(hdl);
-
-				id->hdl = hdl;
-
-				return id;
-			}
-		}
-
-		return NULL;
-	}
-
-	void operator delete(void* p)
-	{
-		if (p)
-		{
-			CCryLobby* pLobby = (CCryLobby*)CCryLobby::GetLobby();
-
-			if (pLobby)
-			{
-				pLobby->MemFree(((SCrySharedUserID*)p)->hdl);
-			}
-		}
-	}
-
-	TMemHdl hdl;
-};
-
-#define STEAMID_AS_STRING_LENGTH (32)
-static CryFixedStringT<STEAMID_AS_STRING_LENGTH> CSteamIDAsString(const CSteamID& id)
-{
-	CryFixedStringT<STEAMID_AS_STRING_LENGTH> result;
-	result.Format("%02X:%X:%05X:%08X", id.GetEUniverse(), id.GetEAccountType(), id.GetUnAccountInstance(), id.GetAccountID());
-
-	return result;
-};
-
-struct SCryLobbyDedicatedServerSetupData
-{
-	CCryLobbyPacket* pPacket;
-	CrySessionHandle session;
-};
-
-struct SteamUserID : public SCrySharedUserID
-{
-	SteamUserID()
-		: m_steamID(CSteamID())
-	{}
-
-	SteamUserID(const CSteamID& steamID)
-		: m_steamID(steamID)
-	{}
-
-	virtual bool operator==(const SCryUserID& other) const
-	{
-		return (m_steamID == static_cast<const SteamUserID&>(other).m_steamID);
-	}
-
-	virtual bool operator<(const SCryUserID& other) const
-	{
-		return (m_steamID < static_cast<const SteamUserID&>(other).m_steamID);
-	}
-
-	virtual CryFixedStringT<CRYLOBBY_USER_GUID_STRING_LENGTH> GetGUIDAsString() const
-	{
-		CryFixedStringT<CRYLOBBY_USER_GUID_STRING_LENGTH> result(CSteamIDAsString(m_steamID));
-		return result;
-	};
-
-	void WriteToPacket(CCryLobbyPacket* pPacket) const
-	{
-		pPacket->WriteUINT64(m_steamID.ConvertToUint64());
-	}
-
-	void ReadFromPacket(CCryLobbyPacket* pPacket)
-	{
-		m_steamID.SetFromUint64(pPacket->ReadUINT64());
-	}
-
-	SteamUserID& operator=(const SteamUserID& other)
-	{
-		m_steamID = other.m_steamID;
-		return *this;
-	}
-
-	CSteamID m_steamID;
-};
-
+#include <../../CryPlugins/CryLobby/Module/CryLobby.h>
+struct CryUserID;
 struct LobbyMenuItem_t
 {
 	/* Probably need to change this wording for enum.... */
@@ -151,29 +34,61 @@ struct LobbyMenuItem_t
 	ELobbyMenuCommand m_eCommand;
 	CSteamID m_steamIDLobby;	// set if k_ELobbyMenuItemInviteToLobby	
 };
+class SteamLobbyService : public CCryLobbyService
+{
+public:
+	SteamLobbyService(CCryLobby* pLobby, ECryLobbyService service);
+	virtual ~SteamLobbyService(void);
 
+	virtual ECryLobbyError   Initialise(ECryLobbyServiceFeatures features, CryLobbyServiceCallback pCB);
+	virtual ECryLobbyError   Terminate(ECryLobbyServiceFeatures features, CryLobbyServiceCallback pCB);
+	virtual void             Tick(CTimeValue tv);
+	virtual ICryMatchMaking* GetMatchMaking();
+	virtual CryUserID      GetUserID(uint32 user) { return CryUserInvalidID; }
+	virtual ECryLobbyError GetSystemTime(uint32 user, SCrySystemTime* pSystemTime);
+
+	virtual void           OnPacket(const TNetAddress& addr, CCryLobbyPacket* pPacket);
+	virtual void           OnError(const TNetAddress& addr, ESocketError error, CryLobbySendID sendID);
+	virtual void           OnSendComplete(const TNetAddress& addr, CryLobbySendID sendID);
+
+	virtual void           GetSocketPorts(uint16& connectPort, uint16& listenPort);
+
+	void                   InviteAccepted(uint32 user, CrySessionID id);
+
+private:
+#if !defined(RELEASE)
+	AppId_t m_SteamAppID;
+#endif // !defined(RELEASE)
+};
+class CCryLobby;
 // an item in the list of lobbies we've found to display
 struct Lobby_t
 {
 	CSteamID m_steamIDLobby;
 	char m_rgchName[256];
 };
-
 typedef void(*CryLobbyServiceCallback)(ECryLobbyError error, CCryLobby* arg, ECryLobbyService service);
-
-class CSteamLobbySystem
+class SteamLobbySystem
 {
 
 
 public:
 	ECryLobbyError Initialise(ECryLobbyServiceFeatures features, CryLobbyServiceCallback pCB);
-	~CSteamLobbySystem(void);
-	CSteamLobbySystem();
-	inline ECryLobbyError CSteamLobbySystem::Initialise(ECryLobbyServiceFeatures features, CryLobbyServiceCallback pCB)
+	~SteamLobbySystem(void);
+	SteamLobbySystem();
+	inline ECryLobbyError SteamLobbySystem::Initialise(ECryLobbyServiceFeatures features, CryLobbyServiceCallback pCB)
 	{
 		return ECryLobbyError();
 	}
-	virtual ~CSteamLobbySystem() {}
+	virtual ~SteamLobbySystem() {}
+
+	inline SteamLobbySystem::SteamLobbySystem()
+	{
+	}
+
+	inline void SteamLobbySystem::SetLobbySteamID(const CSteamID & steamIDLobby)
+	{
+	}
 
 	/*
 		Lobby
